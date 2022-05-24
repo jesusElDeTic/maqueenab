9544badff1f8969338b1bd31d2f518f4b7a53656
jesusElDeTic <74316857+jesusElDeTic@users.noreply.github.com>
2022-05-24 09:34:46 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 128)
+    basic.pause(2000)
+    maqueen.motorStop(maqueen.Motors.All)
+})
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+basic.pause(1000)
+maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+basic.pause(1000)
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+basic.pause(1000)
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+basic.pause(1000)
+maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+basic.forever(function () {
+	
+})
